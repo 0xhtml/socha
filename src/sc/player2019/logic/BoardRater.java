720b961d1676bb6a917aa91b09d1638d5b03cfd1
0xhtml <34682885+0xhtml@users.noreply.github.com>
2019-03-09 12:24:30 +0100

    Optimierungen

--- src/sc/player2019/logic/BoardRater.java
+++ src/sc/player2019/logic/BoardRater.java
@@ -1,5 +1,8 @@
 package sc.player2019.logic;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import sc.plugin2019.Board;
 import sc.plugin2019.Field;
 import sc.plugin2019.FieldState;
@@ -11,63 +14,61 @@ public class BoardRater {
 
 	private int turn;
 
-	private int myPiranhasCount = 0;
-	private int otherPiranhasCount = 0;
+	private int ownPiranhasCount = 0;
+	private int oppPiranhasCount = 0;
 
-	private int myPiranhasPosition = 0;
-	private int otherPiranhasPosition = 0;
+	private int ownPiranhasPosition = 0;
+	private int oppPiranhasPosition = 0;
 
-	private int myPiranhasAtBorder = 0;
-	private int otherPiranhasAtBorder = 0;
+	private int ownPiranhasAtBorder = 0;
+	private int oppPiranhasAtBorder = 0;
 
-	private int myGreatestSwarmSize = 0;
-	private int otherGreatestSwarmSize = 0;
+	private int ownGreatestSwarmSize = 0;
+	private int oppGreatestSwarmSize = 0;
 
-	private int myNotInGreatestSwarmPiranhas = 0;
-	private int otherNotInGreatestSwarmPiranhas = 0;
+	private int ownNotInGreatestSwarmPiranhas = 0;
+	private int oppNotInGreatestSwarmPiranhas = 0;
 
 	public BoardRater(Board board, PlayerColor playerColor, int turn) {
 		this.turn = turn;
 
+		Set<Field> ownPiranhas = new HashSet<>();
+		Set<Field> oppPiranhas = new HashSet<>();
+		
 		for (int x = 0; x < Constants.BOARD_SIZE; x++) {
 			for (int y = 0; y < Constants.BOARD_SIZE; y++) {
 				Field field = board.getField(x, y);
 
-				// Überspringen, wenn das aktuelle Feld kein Fisch ist.
 				if (field.getState() != FieldState.RED && field.getState() != FieldState.BLUE) {
 					continue;
 				}
 
-				// Die Fische zählen
 				if (field.getState().toString() == playerColor.toString()) {
-					myPiranhasCount++;
+					ownPiranhas.add(field);
+					ownPiranhasPosition += evaluatePosition(x, y);
 				} else {
-					otherPiranhasCount++;
+					oppPiranhas.add(field);
+					oppPiranhasPosition += evaluatePosition(x, y);
 				}
-
-				// Zählen der Fische am Rand
+				
 				if (x == 0 || x == Constants.BOARD_SIZE - 1 || y == 0 || y == Constants.BOARD_SIZE - 1) {
 					if (field.getState().toString() == playerColor.toString()) {
-						myPiranhasAtBorder++;
+						ownPiranhasAtBorder++;
 					} else {
-						otherPiranhasAtBorder++;
+						oppPiranhasAtBorder++;
 					}
-				}
-
-				// Die Fischposition bewerten.
-				if (field.getState().toString() == playerColor.toString()) {
-					myPiranhasPosition += evaluatePosition(x, y);
-				} else {
-					otherPiranhasPosition += evaluatePosition(x, y);
 				}
 			}
 		}
 
-		// Größter Schwarm
-		myGreatestSwarmSize = GameRuleLogic.greatestSwarmSize(board, playerColor);
-		otherGreatestSwarmSize = GameRuleLogic.greatestSwarmSize(board, playerColor.opponent());
-		myNotInGreatestSwarmPiranhas = myPiranhasCount - myGreatestSwarmSize;
-		otherNotInGreatestSwarmPiranhas = otherPiranhasCount - otherGreatestSwarmSize;
+		ownPiranhasCount = ownPiranhas.size();
+		oppPiranhasCount = oppPiranhas.size();
+		
+		ownGreatestSwarmSize = GameRuleLogic.greatestSwarmSize(board, ownPiranhas);
+		oppGreatestSwarmSize = GameRuleLogic.greatestSwarmSize(board, oppPiranhas);
+		
+		ownNotInGreatestSwarmPiranhas = ownPiranhasCount - ownGreatestSwarmSize;
+		oppNotInGreatestSwarmPiranhas = oppPiranhasCount - oppGreatestSwarmSize;
 	}
 
 	public double evaluate() {
@@ -78,7 +79,7 @@ public class BoardRater {
 		***REMOVED***
 		***REMOVED***
 		***REMOVED***
-
+		
 		return result;
 	}
 
